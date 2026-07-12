48d15181f5179f2be3baada50c4aec25d57f7a5a
Muhammad Farooq <enmuhammadfarooq@gmail.com>
2026-07-12 13:06:43 +0500

    12-07-2026

--- DSA/07-07-2026/problems.cpp
+++ DSA/07-07-2026/problems.cpp
@@ -345,3 +345,265 @@
 //         return prev;
 //     }
 // };
+
+// T.C => O(n)
+// S.C => O(1)
+
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         // if list is empty or the next node is null then we don't have to reverse the list
+//         if (head == nullptr || head->next == nullptr)
+//             return head;
+
+//         // it is saying that please reverse the rest of list not me
+//         ListNode* newHead = reverseList(head->next);
+
+//         // point the next.next back to current node
+//         // like 4->3
+//         head->next->next = head;
+
+//         // break the old connection
+//         // means 3 still points to 4 so remvoe this connection
+//         head->next = nullptr;
+
+//         // it will return new head which the last node of reverse list points to null
+//         return newHead;
+//     }
+// };
+
+// https://leetcode.com/problems/partition-list/
+
+// T.C => O(n)
+// S.C => O(n)
+
+// class Solution {
+// public:
+//     ListNode* reverseLinkList(ListNode* head) {
+//         if (head == nullptr || head->next == nullptr) {
+//             return head;
+//         }
+
+//         ListNode* newHead = reverseLinkList(head->next);
+
+//         head->next->next = head;
+//         head->next = nullptr;
+//         return newHead;
+//     };
+
+//     ListNode* partition(ListNode* head, int x) {
+//         stack<int> linkedListStack;
+//         ListNode* curr = head;
+
+//         // first push those nodes those are less than x
+//         while (curr != NULL) {
+//             if (curr->val < x) {
+//                 linkedListStack.push(curr->val);
+//             };
+//             curr = curr->next;
+//         }
+//         // cout << linkedListStack.top();
+
+//         curr = head;
+//         // second push those nodes those are greater than or equal to x
+//         while (curr != NULL) {
+//             if (curr->val >= x) {
+//                 linkedListStack.push(curr->val);
+//             };
+//             curr = curr->next;
+//         }
+//         // cout << linkedListStack.top();
+
+//         ListNode* dummyNode = new ListNode(-1);
+//         ListNode* currPtrDummyNode = dummyNode;
+
+//         // pick up the nodes from stack according to LIFO principle and create a linked list but it is in the reverse order because we are first push the less nodes than x and then greater than or equal to x and according to problem statement less nodes should be before the greater than or eqaul to nodes
+//         while (!linkedListStack.empty()) {
+//             currPtrDummyNode->next = new ListNode(linkedListStack.top());
+//             linkedListStack.pop();
+//             currPtrDummyNode = currPtrDummyNode->next;
+//         }
+        
+//         // finally reversing it
+//         return reverseLinkList(dummyNode->next);
+//     }
+// };
+
+// T.C => O(n)
+// S.C => O(n)
+
+// class Solution {
+// public:
+//     ListNode* partition(ListNode* head, int x) {
+//         ListNode* small  = new ListNode(0);
+//         ListNode* large  = new ListNode(0);
+        
+//         ListNode* smallP = small;
+//         ListNode* largeP = large;
+        
+//         while(head) {
+//             if(head->val < x) {
+//                 smallP->next = head;
+//                 smallP = smallP->next;
+//             } else {
+//                 largeP->next = head;
+//                 largeP = largeP->next;
+//             }
+            
+//             head = head->next;
+//         }
+        
+//         largeP->next = NULL;
+//         smallP->next = large->next;
+//         return small->next;
+//     }
+// };
+
+// https://leetcode.com/problems/merge-in-between-linked-lists/
+
+// T.C => O(n)
+// S.C => O(n)
+
+// class Solution {
+// public:
+//     ListNode* reverseLinkList(ListNode* head) {
+//         if (head == nullptr || head->next == nullptr) {
+//             return head;
+//         }
+
+//         ListNode* newHead = reverseLinkList(head->next);
+
+//         head->next->next = head;
+//         head->next = nullptr;
+//         return newHead;
+//     };
+
+//     ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
+//         stack<int> l1Stack;
+//         stack<int> l2Stack;
+//         ListNode* curr = list1;
+
+//         while (curr != NULL) {
+//             l1Stack.push(curr->val);
+//             curr = curr->next;
+//         }
+
+//         curr = list2;
+//         while (curr != NULL) {
+//             l2Stack.push(curr->val);
+//             curr = curr->next;
+//         }
+
+//         ListNode* dummyNode = new ListNode(-1);
+//         ListNode* currPtr = dummyNode;
+//         bool isRemove = false;
+//         int count = l1Stack.size() - 1;
+
+//         while (!l1Stack.empty()) {
+//             if (count >= a && count <= b) {
+//                 isRemove = true;
+//                 l1Stack.pop();
+//                 count--;
+//             };
+
+//             if (isRemove) {
+//                 while (!l2Stack.empty()) {
+//                     currPtr->next = new ListNode(l2Stack.top());
+//                     l2Stack.pop();
+//                     currPtr = currPtr->next;
+//                 }
+//                 isRemove = false;
+//             } else {
+//                 currPtr->next = new ListNode(l1Stack.top());
+//                 l1Stack.pop();
+//                 currPtr = currPtr->next;
+//                 count--;
+//             }
+//         }
+
+//         return reverseLinkList(dummyNode->next);
+//     }
+// };
+
+// T.C => O(n+m)
+// S.C => O(n+m)
+
+// class Solution {
+// public:
+//     ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
+//         ListNode* left = new ListNode(-1);
+//         ListNode* right = new ListNode(-1);
+//         ListNode* leftPtr = left;
+//         ListNode* rightPtr = right;
+
+//         ListNode* curr = list1;
+
+//         int count = 0;
+//         bool isRemoval = false;
+
+//         while (curr) {
+//             if (count >= a && count <= b) {
+//                 isRemoval = true;
+//             };
+
+//             if (isRemoval) {
+//                 while (list2) {
+//                     leftPtr->next = new ListNode(list2->val);
+//                     list2 = list2->next;
+//                     leftPtr = leftPtr->next;
+//                 };
+//                 isRemoval = false;
+//             } else if (count > b) {
+//                 rightPtr->next = new ListNode(curr->val);
+//                 rightPtr = rightPtr->next;
+//             } else {
+//                 leftPtr->next = new ListNode(curr->val);
+//                 leftPtr = leftPtr->next;
+//             };
+
+//             count++;
+//             curr = curr->next;
+//         }
+
+//         cout << left->next->val << endl;
+//         cout << right->next->val << endl;
+//         leftPtr->next = right->next;
+
+//         return left->next;
+//     }
+// };
+
+// T.C => O(n)
+// S.C => O(1)
+
+// class Solution {
+// public:
+//     ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
+//         ListNode* leftPart = NULL;
+//         ListNode* rightPart = list1;
+
+//         // for pointing both leftPart and rightPart nodes
+//         // less than b because we are moving like right->next
+//         for (int i = 0; i <= b; i++) {
+//             if (i == a - 1) {
+//                 leftPart = rightPart;
+//             };
+            
+//             rightPart = rightPart->next;
+//         }
+
+//         // attaching leftPart with list2
+//         leftPart->next = list2;
+
+//         ListNode* temp = list2;
+//         // now going to last node of list2
+//         while (temp && temp->next) {
+//             temp = temp->next;
+//         }
+
+//         // when on last node we will attach rightPart
+//         temp->next = rightPart;
+
+//         return list1;
+//     }
+// };
