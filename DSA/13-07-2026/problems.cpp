75970e5229dceca783104d0b1a9c332d948b6cbc
Muhammad Farooq <enmuhammadfarooq@gmail.com>
2026-07-19 21:42:14 +0500

    19-07-2026

--- DSA/13-07-2026/problems.cpp
+++ DSA/13-07-2026/problems.cpp
@@ -371,3 +371,36 @@
 //         return ans;
 //     }
 // };
+
+// https://leetcode.com/problems/find-kth-bit-in-nth-binary-string/
+
+// T.C => O(n)
+// S.C => O(n)
+
+// class Solution
+// {
+// public:
+//     char findKthBit(int n, int k)
+//     {
+//         if (n == 1)
+//         {
+//             return '0';
+//         };
+
+//         int lengthOfStr = pow(2, n) - 1;
+
+//         // left:
+//         if (k < ceil(lengthOfStr / 2.0))
+//         {
+//             return findKthBit(n - 1, k);
+//         }
+//         else if (k > ceil(lengthOfStr / 2.0))
+//         {
+//             // right:
+//             return findKthBit(n - 1, lengthOfStr - (k - 1)) == '0' ? '1' : '0';
+//         };
+
+//         // mid:
+//         return '1';
+//     }
+// };
