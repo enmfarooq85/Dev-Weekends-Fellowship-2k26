85f9019189643c2838aa364f1db168c88ca59dbb
Muhammad Farooq <enmuhammadfarooq@gmail.com>
2026-08-04 22:36:15 +0500

    04-08-2026

--- DSA/20-07-2026/problems.cpp
+++ DSA/20-07-2026/problems.cpp
@@ -1027,3 +1027,34 @@
 //         return solve(0, arr, temp, n);
 //     }
 // };
+
+// https://leetcode.com/problems/minimum-insertion-steps-to-make-a-string-palindrome/
+
+// T.C => O(n * n)
+// S.C => O(n * n) 
+
+// class Solution {
+// public:
+//     int dp[501][501];
+//     int solve(int i, int j, string& s) {
+
+//         if (i >= j)
+//             return 0;
+
+//         if (dp[i][j] != -1)
+//             return dp[i][j];
+
+//         if (s[i] == s[j])
+//             return dp[i][j] = solve(i + 1, j - 1, s);
+//         else
+//             return dp[i][j] = 1 + min(solve(i, j - 1, s), solve(i + 1, j, s));
+//     }
+
+//     int minInsertions(string s) {
+//         int n = s.length();
+
+//         memset(dp, -1, sizeof(dp));
+
+//         return solve(0, n - 1, s);
+//     }
+// };
