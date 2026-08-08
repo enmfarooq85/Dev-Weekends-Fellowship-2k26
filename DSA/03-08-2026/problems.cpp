350e88ef80a9985ea1362112884ea1acf11c5f97
Muhammad Farooq <enmuhammadfarooq@gmail.com>
2026-08-08 22:41:05 +0500

    08-08-2026

--- DSA/03-08-2026/problems.cpp
+++ DSA/03-08-2026/problems.cpp
@@ -192,7 +192,6 @@
 
 // https://leetcode.com/problems/arithmetic-slices-ii-subsequence/
 
-
 // T.C => O(n^2)
 // S.C => O(n^2)
 
@@ -215,5 +214,35 @@
 //             }
 //         }
 //         return result;
+//     }
+// };
+
+// https://leetcode.com/problems/longest-common-subsequence/
+
+// T.C => O(n*m)
+// S.C => O(n)
+
+// class Solution {
+// public:
+//     int t[1001][1001];
+//     int LCS(string& s1, string& s2, int m, int n) {
+//         if(m == 0 || n == 0)
+//             return t[m][n] = 0;
+        
+//         if(t[m][n] != -1)
+//             return t[m][n];
+        
+//         if(s1[m-1] == s2[n-1])
+//             return t[m][n] = 1 + LCS(s1, s2, m-1, n-1);
+        
+//         return t[m][n] = max(LCS(s1, s2, m, n-1), LCS(s1, s2, m-1, n));
+//     }
+//     int longestCommonSubsequence(string text1, string text2) {
+//         int m = text1.length();
+//         int n = text2.length();
+        
+//         memset(t, -1, sizeof(t));
+        
+//         return LCS(text1, text2, m, n);
 //     }
 // };
