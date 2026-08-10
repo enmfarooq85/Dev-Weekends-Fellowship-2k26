c5b207d2061d5f1617df471e5dc5cd6f6ae0b82f
Muhammad Farooq <enmuhammadfarooq@gmail.com>
2026-08-10 13:16:24 +0500

    10-08-2026

--- DSA/03-08-2026/problems.cpp
+++ DSA/03-08-2026/problems.cpp
@@ -339,6 +339,32 @@
 //     };
 // };
 
+// T.C => O(m*n)
+// S.C => O(m*n)
+
+// class Solution {
+// public:
+//     int longestCommonSubsequence(string text1, string text2) {
+//         int m = text1.length();
+//         int n = text2.length();
+
+//         vector<vector<int>> t(m + 1, vector<int>(n + 1));
+
+//         for (int i = 0; i < m + 1; i++) {
+//             for (int j = 0; j < n + 1; j++) {
+//                 if (i == 0 || j == 0)
+//                     t[i][j] = 0;
+//                 else if (text1[i - 1] == text2[j - 1])
+//                     t[i][j] = 1 + t[i - 1][j - 1];
+//                 else
+//                     t[i][j] = max(t[i][j - 1], t[i - 1][j]);
+//             }
+//         }
+
+//         return t[m][n];
+//     }
+// };
+
 // https://leetcode.com/problems/ugly-number/description/
 
 // T.C => O(n * log n)
@@ -441,5 +467,58 @@
 //         }
 
 //         return ans[n];
+//     }
+// };
+
+// https://leetcode.com/problems/domino-and-tromino-tiling/
+
+// T.C => O(n)
+// S.C => O(n)
+
+// class Solution {
+// public:
+//     int M = 1000000007;
+//     int cache[1001];
+//     int solve(int n) {
+//         if (n == 1 || n == 2)
+//             return n;
+//         if (n == 3)
+//             return 5;
+
+//         if (cache[n] != -1)
+//             return cache[n];
+        
+//         // 2 * (n -1) + (n - 3):
+//         return cache[n] = (2 * solve(n - 1) % M + solve(n - 3) % M) % M;
+//     }
+
+//     int numTilings(int n) {
+//         memset(cache, -1, sizeof(cache));
+
+//         return solve(n);
+//     }
+// };
+
+// T.C => O(n)
+// S.C => O(n)
+
+// class Solution {
+// public:
+//     int M = 1000000007;
+//     int numTilings(int n) {
+//         if (n == 1 || n == 2)
+//             return n;
+
+//         vector<int> arr(n + 1, 0);
+
+//         arr[1] = 1;
+//         arr[2] = 2;
+//         arr[3] = 5;
+
+//         for (int i = 4; i <= n; i++) {
+//             arr[i] = (2 * arr[i - 1] % M + arr[i - 3] % M) % M;
+//         }
+
+//         return arr[n];
 //     }
 // };
