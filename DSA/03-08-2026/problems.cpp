2e65d6ffddb4ca1cfc36dc7a673ad1e8a0478496
Muhammad Farooq <enmuhammadfarooq@gmail.com>
2026-08-13 10:28:52 +0500

    13-08-2026

--- DSA/03-08-2026/problems.cpp
+++ DSA/03-08-2026/problems.cpp
@@ -719,3 +719,89 @@
 //         return ans;
 //     }
 // };
+
+// https://leetcode.com/problems/scramble-string
+
+// T.C => O(2^n)
+// S.C => O(n)
+
+// class Solution {
+// public:
+//     bool solve(string s1, string s2) {
+
+//         if (s1 == s2)
+//             return true;
+
+//         if (s1.length() != s2.length())
+//             return false;
+
+//         bool result = false;
+//         int n = s1.length();
+//         for (int i = 1; i < n; i++) {
+
+//             bool swapped = solve(s1.substr(0, i), s2.substr(n - i, i)) &&
+//                            solve(s1.substr(i, n - i), s2.substr(0, n - i));
+//             if (swapped) {
+//                 result = true;
+//                 break;
+//             }
+
+//             bool not_swapped = solve(s1.substr(0, i), s2.substr(0, i)) &&
+//                                solve(s1.substr(i, n - i), s2.substr(i, n - i));
+//             if (not_swapped) {
+//                 result = true;
+//                 break;
+//             }
+//         }
+
+//         return result;
+//     }
+//     bool isScramble(string& s1, string& s2) {
+//         return solve(s1, s2);
+//     }
+// };
+
+// T.C => O(n)
+// S.C => O(n)
+
+// class Solution {
+// public:
+//     unordered_map<string, bool> mp;
+//     bool solve(string s1, string s2) {
+
+//         if (s1 == s2)
+//             return true;
+
+//         if (s1.length() != s2.length())
+//             return false;
+
+//         string key = s1 + "-" + s2;
+//         if (mp.find(key) != mp.end())
+//             return mp[key];
+
+//         bool result = false;
+//         int n = s1.length();
+//         for (int i = 1; i < n; i++) {
+
+//             bool swapped = solve(s1.substr(0, i), s2.substr(n - i, i)) &&
+//                            solve(s1.substr(i, n - i), s2.substr(0, n - i));
+//             if (swapped) {
+//                 result = true;
+//                 break;
+//             }
+
+//             bool not_swapped = solve(s1.substr(0, i), s2.substr(0, i)) &&
+//                                solve(s1.substr(i, n - i), s2.substr(i, n - i));
+//             if (not_swapped) {
+//                 result = true;
+//                 break;
+//             }
+//         }
+
+//         return mp[key] = result;
+//     }
+//     bool isScramble(string& s1, string& s2) {
+//         mp.clear();
+//         return solve(s1, s2);
+//     }
+// };
