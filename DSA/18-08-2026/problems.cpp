417b25a4586d9efaf93ce71259e47373754f8d01
Muhammad Farooq <enmuhammadfarooq@gmail.com>
2026-08-20 21:20:20 +0500

    20-08-2026

--- DSA/18-08-2026/problems.cpp
+++ DSA/18-08-2026/problems.cpp
@@ -67,3 +67,140 @@
 //         return head;
 //     }
 // };
+
+// https://leetcode.com/problems/lru-cache/
+
+// T.C => O(n^2)
+// S.C => O(n)
+
+// class LRUCache {
+// public:
+//     vector<pair<int, int>> cache;
+//     int n;
+
+//     LRUCache(int capacity) { n = capacity; }
+
+//     int get(int key) {
+//         for (int i = 0; i < cache.size(); i++) {
+//             if (cache[i].first == key) {
+//                 int val = cache[i].second;
+
+//                 pair<int, int> temp = cache[i];
+//                 cache.erase(cache.begin() + i);
+//                 cache.push_back(temp);
+
+//                 return val;
+//             }
+//         }
+
+//         return -1;
+//     }
+
+//     void put(int key, int value) {
+//         for (int i = 0; i < cache.size(); i++) {
+//             if (cache[i].first == key) {
+//                 cache.erase(cache.begin() + i);
+//                 cache.push_back({key, value});
+//                 return;
+//             }
+//         }
+
+//         if (cache.size() == n) {
+//             cache.erase(cache.begin());
+//             cache.push_back({key, value});
+//         } else {
+//             cache.push_back({key, value});
+//         }
+//     }
+// };
+
+// T.C => O(1)
+// S.C => O(n)
+
+// class LRUCache {
+// public:
+//     list<int> doublyLinkedList;
+//     map<int, pair<list<int>::iterator, int>> cache;
+//     int size;
+
+//     LRUCache(int capacity) { size = capacity; }
+
+//     void shiftRecentlyUsedNodeToFront(int key) {
+//         doublyLinkedList.erase(cache[key].first);
+//         doublyLinkedList.push_front(key);
+
+//         cache[key].first = doublyLinkedList.begin();
+//     }
+
+//     int get(int key) {
+//         if (cache.find(key) == cache.end()) {
+//             return -1;
+//         }
+
+//         shiftRecentlyUsedNodeToFront(key);
+
+//         return cache[key].second;
+//     }
+
+//     void put(int key, int value) {
+//         if (cache.find(key) != cache.end()) {
+//             cache[key].second = value;
+//             shiftRecentlyUsedNodeToFront(key);
+//         } else {
+//             doublyLinkedList.push_front(key);
+//             cache[key] = {doublyLinkedList.begin(), value};
+
+//             size--;
+//         }
+
+//         if (size < 0) {
+//             int nodeToDel = doublyLinkedList.back();
+//             cache.erase(nodeToDel);
+//             doublyLinkedList.pop_back();
+//             size++;
+//         }
+//     }
+// };
+
+// https://leetcode.com/problems/split-linked-list-in-parts/
+
+// T.C => O(l + k)
+// S.C => O(1)
+
+// class Solution {
+// public:
+//     vector<ListNode*> splitListToParts(ListNode* head, int k) {
+//         ListNode* curr = head;
+
+//         int length = 0;
+//         while (curr) {
+//             length++;
+//             curr = curr->next;
+//         }
+
+//         int eachBucketNodes = length / k;
+//         int extraNodes = length % k;
+
+//         vector<ListNode*> result(k, NULL);
+//         curr = head;
+//         ListNode* prev = NULL;
+
+//         for (int i = 0; i < k; i++) {
+//             result[i] = curr;
+
+//             for (int count = 1;
+//                  count <= eachBucketNodes + (extraNodes > 0 ? 1 : 0); count++) {
+
+//                 prev = curr;
+//                 curr = curr->next;
+//             }
+
+//             if (prev != NULL) {
+//                 prev->next = NULL;
+//             }
+//             extraNodes--;
+//         }
+
+//         return result;
+//     }
+// };
